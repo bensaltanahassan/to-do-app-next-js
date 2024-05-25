9ebd138aed2f447f5a9a68c61203603937d6f626
bensaltana hassan <bensaltana10@gmail.com>
2024-05-25 22:18:26 +0100

    update pb

--- src/components/todos/Todos.tsx
+++ src/components/todos/Todos.tsx
@@ -44,6 +44,9 @@ export default function Todos() {
   };
 
   const saveTodo = () => {
+    if (!newTodo) {
+      return;
+    }
     const isCompleted = Math.random() < 0.5;
     const newTodoObj: Todo = {
       id: todos.length + 1,
@@ -74,16 +77,28 @@ export default function Todos() {
   }
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center w-screen bg-gray-100 py-24 px-4 flex-col h-screen">
+        Loading...
+      </div>
+    );
   }
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return (
+      <div className="flex items-center justify-center w-screen bg-gray-100 py-24 px-4 flex-col h-screen">
+        Error: {error.message}
+      </div>
+    );
   }
 
   return (
     <div className="flex items-center justify-center w-screen bg-gray-100 py-24 px-4 flex-col">
       <div className="bg-white p-8 rounded shadow-md w-full md:w-2/3 lg:w-1/2 mb-4">
-        <div className="flex flex-row justify-between mb-4">
+        <div
+          className={`flex flex-row justify-between 
+                ${showAddTodo && "mb-4"}
+        `}
+        >
           <div>
             <h1 className="text-2xl font-semibold">Todos</h1>
             <p className="text-gray-500">A list of todos</p>
